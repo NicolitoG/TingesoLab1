0e10ac3162bf09f0656aea34baa8cac88be6af1c
NicolitoG <nicolas.garcia.r@usach.cl>
2024-11-06 23:53:55 -0300

    arreglando cosas del jenkins

--- back-end-1/src/test/java/Trabajo/TingesoNico/TingesoNicoApplicationTests.java
+++ back-end-1/src/test/java/Trabajo/TingesoNico/TingesoNicoApplicationTests.java
@@ -3,7 +3,7 @@ package Trabajo.TingesoNico;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Trabajo.TingesoNico.TingesoNicoApplication.class)
 class TingesoNicoApplicationTests {
 
 	@Test
